b107836c0f995c8f6d7fa9ac9114fe7875f5a388
jindalshivani <sjindal807@gmail.com>
2018-10-11 15:18:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nandkishorjindal/googlemaps/Constants.java
@@ -0,0 +1,6 @@
+package com.example.nandkishorjindal.googlemaps;
+
+public class Constants {
+
+    public static final int ERROR_DIALOG_REQUEST = 9001;
+}
---
+++ app/src/main/java/com/example/nandkishorjindal/googlemaps/ui/IProfile.java
@@ -0,0 +1,6 @@
+package com.example.nandkishorjindal.googlemaps.ui;
+
+public interface IProfile {
+
+    void onImageSelected(int resource);
+}
